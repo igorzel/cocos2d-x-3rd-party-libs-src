ca0864c7c87bfd2a1569f79e8a4592eef909c9dc
igorzel <izeleniy@VIW20357-mac.local>
2015-09-23 15:51:35 +0300

    Android build API changed to 14

--- build/build_websockets_android.sh
+++ build/build_websockets_android.sh
@@ -2,7 +2,7 @@
 
 rm -rf /tmp/temp-standalone-android-toolchain
 
-${ANDROID_NDK}/build/tools/make-standalone-toolchain.sh --platform=android-9 --install-dir=/tmp/temp-standalone-android-toolchain --toolchain=arm-linux-androideabi-4.9
+${ANDROID_NDK}/build/tools/make-standalone-toolchain.sh --platform=android-14 --install-dir=/tmp/temp-standalone-android-toolchain --toolchain=arm-linux-androideabi-4.9
 
 export PATH=/tmp/temp-standalone-android-toolchain/bin:$PATH
 export CC="arm-linux-androideabi-gcc"
